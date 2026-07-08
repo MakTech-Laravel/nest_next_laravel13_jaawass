dcbbb9b5e4907b34c3526457bd1b5c20e6a24137
Md Monirul Islam Taluqdar <mdmonirul.islam.bd2003@gmail.com>
2026-07-08 18:06:18 +0600

    Updated

--- resources/views/mail/rfq-created-manufacturer.blade.php
+++ resources/views/mail/rfq-created-manufacturer.blade.php
@@ -4,7 +4,7 @@
     $frontendUrl = rtrim((string) config('app.frontend_url', config('app.url')), '/');
     $mailIconStyle = 'display:block;border:0;outline:none;text-decoration:none;margin:0 auto;';
     $recipientName = trim($recipientName ?? $name ?? '') !== '' ? trim($recipientName ?? $name ?? '') : 'there';
-    $ctaUrl = $ctaUrl ?? \App\Support\Mail\MailNotificationHelper::frontendUrl('dashboard/manufacturer/rfqs');
+    $ctaUrl = $ctaUrl ?? \App\Support\Mail\MailNotificationHelper::frontendUrl('dashboard/manufacturer/inquiries');
     $inboxUrl = $inboxUrl ?? \App\Support\Mail\MailNotificationHelper::frontendUrl('dashboard/manufacturer/rfqs');
     $heroIconUrl = public_url('images/mail/svg/inquiry-pencil-hero.svg');
     $inquiryTags = $inquiryTags ?? [];
